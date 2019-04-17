80c4070e85d1f5fa3fe20dda1dbc5a3f44b76c80
Andreas Schäfer <k0haku@hotmail.com>
2019-04-17 16:45:36 +0200

    reinit django properly

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-"""main URL Configuration
+"""k0haku URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from notes.views import TestView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', TestView.as_view())
 ]
