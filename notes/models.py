362e64328e93dff1908038425eecd74d58b6145b
Andreas <k0haku@hotmail.com>
2019-04-24 16:09:18 +0200

    add time/description and allows types/tags to be empty

--- notes/models.py
+++ notes/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 
 class Note(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    types = models.ManyToManyField('NoteType')
-    tags = models.ManyToManyField('NoteTag')
+    types = models.ManyToManyField('NoteType', blank=True)
+    tags = models.ManyToManyField('NoteTag', blank=True)
 
 
 class NoteType(models.Model):
     name = models.CharField(max_length=15)
+    description = models.TextField(blank=True)
 
 
 class NoteTag(models.Model):
     name = models.CharField(max_length=15)
+    description = models.TextField(blank=True)
