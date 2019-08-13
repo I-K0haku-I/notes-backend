c6e089975b364e498a2d33ef52f37812afb5287c
Andreas <k0haku@hotmail.com>
2019-08-13 17:10:05 +0200

    Add date filtering

--- notes/api/notes/resources.py
+++ notes/api/notes/resources.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import status, mixins, viewsets
 from rest_framework.response import Response
 
@@ -9,4 +11,12 @@ class NotesViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NotesSerializer
 
-# TODO: add filtering for timeframes, through params?
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        new_date = self.request.query_params.get('date')
+        if new_date is not None:
+            new_date = date.fromisoformat(str(new_date))
+            queryset = queryset.filter(time__date=new_date)
+        return queryset
+
+# TODO: add filtering for timeframe too, only day possible right now
