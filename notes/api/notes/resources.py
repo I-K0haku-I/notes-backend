5a6e5fc2fa69c910104ea5754ecb3a5bdd295e10
Andreas <k0haku@hotmail.com>
2020-01-06 19:06:54 +0100

    Add filter of notes with tag name

--- notes/api/notes/resources.py
+++ notes/api/notes/resources.py
@@ -3,7 +3,7 @@ from datetime import date
 from rest_framework import status, mixins, viewsets
 from rest_framework.response import Response
 
-from notes.models import Note
+from notes.models import Note, NoteTag
 
 from .serializers import NotesSerializer
 
@@ -14,9 +14,15 @@ class NotesViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         new_date = self.request.query_params.get('date')
-        if new_date is not None:
+        if new_date:
+            # checking if date format would be useful, but works without it fine
             new_date = date.fromisoformat(str(new_date))
             queryset = queryset.filter(time__date=new_date)
+        tags = self.request.query_params.get('tags')
+        if tags:
+            tags = tags.split(',')
+            queryset = queryset.filter(tags__name__in=tags)
+
         return queryset
 
 # TODO: add filtering for timeframe too, only day possible right now
