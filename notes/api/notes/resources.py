9e21f94fbf7b075cc7e70aafc9d1a293e8f498bf
Andreas <k0haku@hotmail.com>
2020-01-09 16:39:20 +0100

    Add filter for date range

--- notes/api/notes/resources.py
+++ notes/api/notes/resources.py
@@ -13,6 +13,12 @@ class NotesViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = super().get_queryset()
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        if start_date and end_date:
+            start_date = date.fromisoformat(str(start_date))
+            end_date = date.fromisoformat(str(end_date))
+            queryset = queryset.filter(time__date__range=[start_date, end_date])
         new_date = self.request.query_params.get('date')
         if new_date:
             # checking if date format would be useful, but works without it fine
